07a19bc5d36695be6f9048f78f3a629f054948e0
EcodiaTate <tate@ecodia.au>
2026-03-12 12:05:36 +1000

    fjudfh

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactCompiler: true,
+  output: "export", // <--- This tells Next.js to build the static 'out' folder for Capacitor
 };
 
 export default nextConfig;
